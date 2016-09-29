38a2bd5c7f58b5bf6f08d945d2e33341a3bdf4d1
ikeda <ikebright@gmail.com>
2016-09-29 20:49:21 +0900

    add no-helper and no-assets to noconfig/application.rb

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module FlowSample
       g.assets     false
       g.helper     false
     end
+    
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
